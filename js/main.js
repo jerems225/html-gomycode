c175e3c8e6debc3547132754db81e04b739062bb
Jeremy Niondo <jeremieniondo@gmail.com>
2025-12-20 18:10:03 +0000

    dom creation

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var headerElement = document.querySelector('header');
+var navbarElement = document.querySelector('.header-navbar')
+var menuIcon = document.querySelector('#menu-icon');
+
+console.log(headerElement, navbarElement, menuIcon);
+menuIcon.remove()
